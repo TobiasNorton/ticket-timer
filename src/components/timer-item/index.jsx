1488c565541d8c76d0a200bd6905d8863c146df1
Tobias Norton <tnorton20@yahoo.com>
2019-12-25 16:00:56 -0500

    Initial app setup

---
+++ src/components/timer-item/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TimerItem = () => {
+  return (
+    <div>
+      <h3>REIT-430</h3>
+      <p>Brief description about the ticket</p>
+      <p>4 hours, 34 minutes</p>
+      <button>Stop</button>
+    </div>
+  )
+}
+
+export default TimerItem
---
+++ src/containers/app/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.scss'
+import TimerItem from '../../components/timer-item'
+
+function App() {
+  return (
+    <div className="App">
+      <TimerItem />
+      <TimerItem />
+      <TimerItem />
+      <TimerItem />
+      <TimerItem />
+    </div>
+  )
+}
+
+export default App
