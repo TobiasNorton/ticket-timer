1ab1601f150c1be1708667839f30bc9d28694b36
Tobias Norton <tnorton20@yahoo.com>
2019-12-25 17:19:08 -0500

    Navigation menu, ticket container

--- src/containers/app/index.jsx
+++ src/containers/app/index.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.scss'
+import Tickets from '../tickets'
 import TimerItem from '../../components/timer-item'
+import NavigationMenu from '../../components/navigation-menu'
 
 function App() {
   return (
     <div className="App">
-      <TimerItem />
-      <TimerItem />
-      <TimerItem />
-      <TimerItem />
-      <TimerItem />
+      <NavigationMenu />
+      <Router>
+        <div>
+          <Route exact path="/" component={Tickets} />
+          {/* <Route path="/archives" component={Archives} /> */}
+        </div>
+      </Router>
     </div>
   )
 }
---
+++ src/containers/tickets/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import TimerItem from '../../components/timer-item'
+
+const index = () => {
+  return (
+    <div>
+      <TimerItem />
+      <TimerItem />
+      <TimerItem />
+      <TimerItem />
+      <TimerItem />
+    </div>
+  )
+}
+
+export default index
